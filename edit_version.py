0b43a31377fdfc4543469caeda8e42f69fb39846
tuxisawesome <67063826+tuxisawesome@users.noreply.github.com>
2022-08-09 12:14:24 -0400

    Da G-I-T

---
+++ edit_version.py
@@ -0,0 +1,12 @@
+import os
+print("WalterOS-Linux-TUI [BOT]")
+with open("version.txt", "r") as r:
+    currentversion = r.readline()
+    print(f"* {currentversion} is the current version")
+    r.close()
+with open("version.txt", "w") as w:
+    latestversion = str(int(currentversion) + 0.01)
+    print(f"* changed version to {latestversion}")
+    w.write(latestversion)
+    w.close()
+print(":-> Update pushed")
